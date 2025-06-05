0bfc8cefee6d5823793913ec2903a9ccba1412b7
Juan Lagunas <dev.jlagunas@gmail.com>
2025-06-05 14:21:27 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="bg-primary border-line text-white">
+    I'm using theme tokens as native Tailwind classes.
+  </div>
+</template>
+
+<style scoped></style>
