4ed64a8775ee20740127246678ca7a708d6f44a6
Juan Lagunas <dev.jlagunas@gmail.com>
2025-06-06 16:48:49 -0700

    feat: set up HomeFooter component foundation and styling

---
+++ src/components/HomeFooter.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="font-headings text-center">
+    <h1 class="text-8xl font-bold tracking-tight">
+      <span class="font-bold text-pink">L</span>et's <span class="text-blue">G</span>et
+      <span class="text-pink">T</span>o <span class="text-blue">W</span>ork!
+    </h1>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import TheHero from '@/components/TheHero.vue'
 import HomeProjects from '@/components/HomeProjects.vue'
 import ArrowTransition from '@/components/reusable/ArrowPointer.vue'
+import HomeFooter from '@/components/HomeFooter.vue'
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import ArrowTransition from '@/components/reusable/ArrowPointer.vue'
   <ArrowTransition customClass="md:-mt-6  md:rotate-45 lg:-rotate-45 lg:-mt-24 lg:-ml-64" />
   <HomeProjects />
   <ArrowTransition customClass="md:mt-5 md:-rotate-45 lg:rotate-45 lg:-ml-50" />
+  <HomeFooter />
 </template>
 
 <style scoped></style>
