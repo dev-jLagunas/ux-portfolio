dbff0102c80cf20e80d5fdeacaf6e59d1111e7b0
Juan Lagunas <dev.jlagunas@gmail.com>
2025-06-06 17:27:53 -0700

    feat: setup foundational html and styling for LinkCar.vue

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,12 +1,22 @@
-<script setup></script>
+<script setup>
+import LinkCard from '@/components/reusable/LinkCard.vue'
+</script>
 
 <template>
-  <div class="font-headings text-center">
-    <h1 class="text-8xl font-bold tracking-tight">
-      <span class="font-bold text-pink">L</span>et's <span class="text-blue">G</span>et
-      <span class="text-pink">T</span>o <span class="text-blue">W</span>ork!
-    </h1>
-  </div>
+  <footer>
+    <div class="font-headings text-center">
+      <h1 class="text-8xl font-bold tracking-tight">
+        <span class="font-bold text-pink">L</span>et's <span class="text-blue">G</span>et
+        <span class="text-pink">T</span>o <span class="text-blue">W</span>ork!
+      </h1>
+    </div>
+    <section class="flex flex-wrap justify-center gap-4">
+      <LinkCard />
+      <LinkCard />
+      <LinkCard />
+      <LinkCard />
+    </section>
+  </footer>
 </template>
 
 <style scoped></style>
---
+++ src/components/reusable/LinkCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+import instagramIcon from '@/assets/svgs/instagram-icon.svg'
+</script>
+
+<template>
+  <article
+    class="rounded-md h-fit p-4 font-headings flex flex-col justify-center items-center w-fit mt-8 border border-gray-200"
+  >
+    <h2 class="font-bold">My Instagram</h2>
+    <img :src="instagramIcon" alt="" class="h-20 pb-2" />
+    <button class="text-sm border-2 border-pink p-2 rounded-sm tracking-widest">See Now</button>
+  </article>
+</template>
+
+<style scoped></style>
