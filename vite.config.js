312d57c8bdf30fa2ccea4bc17ab96a8ad489bfc8
Juan Lagunas <dev.jlagunas@gmail.com>
2025-07-12 14:19:06 -0700

    add / base to vite.config.s

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ux-portfolio/',
+  base: '/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
