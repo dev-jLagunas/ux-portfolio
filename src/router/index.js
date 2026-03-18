6ed01a62d94b76179c99b01282a624142a8766a3
Juan Lagunas <dev.jlagunas@gmail.com>
2026-03-18 14:57:34 +0900

    add google analytics to site

--- src/router/index.js
+++ src/router/index.js
@@ -64,4 +64,14 @@ const router = createRouter({
   },
 })
 
+router.afterEach((to) => {
+  if (window.gtag) {
+    window.gtag('event', 'page_view', {
+      page_title: document.title,
+      page_location: window.location.href,
+      page_path: to.fullPath,
+    })
+  }
+})
+
 export default router
