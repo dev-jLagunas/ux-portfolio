d74cd7222c623ead114a81b2b91b98f4637b5b32
Juan Lagunas <dev.jlagunas@gmail.com>
2026-04-23 14:37:37 +0900

    Updates

--- assets/createVueComponent-CUcKdeSk.js
+++ assets/createVueComponent-CUcKdeSk.js
@@ -1,4 +1,4 @@
-import{H as r}from"./index-BrhW8Z4l.js";/**
+import{H as r}from"./index-C5ubxPxr.js";/**
  * @license @tabler/icons-vue v3.34.0 - MIT
  *
  * This source code is licensed under the MIT license.
